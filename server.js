11ddaddabe315a852262a67923b02769b37fbe3f
ChanKai-crypto <killian.chan.2023@gmail.com>
2026-09-11 21:29:18 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const authRoutes = require("./src/routes/auth");
 const userRoutes = require("./src/routes/users");
 const quizRoutes = require("./src/routes/quizzes");
 const adminRoutes = require("./src/routes/admin");
+const playlistRoutes = require("./src/routes/playlists");
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/quizzes", quizRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/playlists", playlistRoutes);
 
 // 404 générique pour toute route /api/* non reconnue
 app.use("/api", (req, res) => {
